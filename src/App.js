8a0e1f6bed05a6fc0d4a6c52ddba40b59f52a0cb
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-08-27 23:24:24 -0400

    working out logic of game setting state

--- src/App.js
+++ src/App.js
@@ -7,24 +7,50 @@ import cards from "./cards.json";
 class App extends Component {
   // Setting this.state.cards to the cards json array
   state = {
-    cards
+    cards: cards,
+    clickedCards: [],
+    highScore: 0
   };
 
-  cardClicked = id => {
+  cardClicked = id => { 
+
+    if(this.state.clickedCards.id === id) {
+      alert("game Over")
+      //then reset the state
+    }
+    let currentIndex = cards.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = cards[currentIndex];
+      cards[currentIndex] = cards[randomIndex];
+      cards[randomIndex] = temporaryValue;
+    }
     // Filter this.state.cards for cards with an id not equal to the id being removed
-    const cards = this.state.cards.filter(card => card.id !== id);
+    // const cards = this.state.cards.filter(card => card.id !== id);
+    // const clickedCards = this.state.cards.filter(card => card.id === id);
+    const clickedCards = this.state.cards.filter(card => card.id === id);
     // Set this.state.cards equal to the new cards array
-    this.setState({ cards });
+    // this.setState({clickedCards: clickedCards})
+    this.setState({ cards: cards, clickedCards: clickedCards });
+    this.setState({clickedCards: [...this.state.clickedCards, clickedCards]})
   };
 
+  
   // Map over this.state.cards and render a cardCard component for each card object
   render() {
+    console.log(this.state.cards)
+    console.log(this.state.clickedCards)
     return (
       <Wrapper>
         <Title>Clicky Game</Title>
         {this.state.cards.map(card => (
           <PlayingCard
             cardClicked={this.cardClicked}
+            shuffle={this.shuffle}
             id={card.id}
             key={card.id}
             name={card.name}
